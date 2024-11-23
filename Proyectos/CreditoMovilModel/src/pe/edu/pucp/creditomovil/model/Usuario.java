ae4d23e2987aaa73256063d0c0c033b44a0e9bbb
aaaron47 <aaronarana29@gmail.com>
2024-11-23 16:28:21 -0500

    .

--- Proyectos/CreditoMovilModel/src/pe/edu/pucp/creditomovil/model/Usuario.java
+++ Proyectos/CreditoMovilModel/src/pe/edu/pucp/creditomovil/model/Usuario.java
@@ -20,6 +20,7 @@ public abstract class Usuario {
     private TipoDocumento tipoDocumento;
     private String documento;
     private Rol rol;
+    private String notificacion;
     
     public Usuario(){}
     public Usuario(int idUsuario, Date fecha, String nombre, String apPaterno,
@@ -153,5 +154,13 @@ public abstract class Usuario {
 
     public void setRol(Rol rol) {
         this.rol = rol;
+    }
+    
+    public void setNotificacion(String notificacion) {
+        this.notificacion = notificacion;
+    }
+
+    public String getNotificacion() {
+        return notificacion;
     }
 }
